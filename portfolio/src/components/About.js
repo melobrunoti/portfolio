3bfbc59eab078fa389a0c30f432eebf587c39fd3
melobrunoti <melobrunoti@gmail.com>
2022-05-13 09:04:55 -0300

    about section in progress

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import iconSet from "../images/selection.json";
+import IcomoonReact, { iconList } from "icomoon-react";
+import skills from '../data/skills'
 
 export default function About() {
-  return (
-  <div className='about'>
-    <h2>Sobre mim</h2>
-    <p>Estudante de desenvolvimento web na trybe ,apaixonado por tecnologia focado em me tornar um grande desenvolvedor ! </p>
-  </div>
+ return (   
+  <div className='about-section'>
+    <div className='about' >
+      <h2 className="about__title">Sobre mim</h2>
+      <p className='about__paragraph' >Olá, Meu nome é Bruno Melo, sou desenvolvedor Front end e estudante de desenvolvimento web full stack na trybe.</p>
+      <p className='about__paragraph' >Apaixonado por tecnologia, 100% focado em aprimorar e adquirir novos conhecimentos! </p>
+    </div>
+    <div className='skills' >
+      
+      {skills.map(({ name, icon }, index) => 
+        <div key={index} className='skill__box' >
+         <IcomoonReact iconSet={iconSet} size={50} icon={icon}/>
+         <span>{name}</span>
+         </div>
+      )}
+     
+      </div>
+    </div>
   )
 }
---
+++ portfolio/src/data/skills.js
@@ -0,0 +1,40 @@
+const skills = [
+  {
+    icon:'sass',
+    name:'Sass'
+  },
+  {
+    icon:'css3',
+    name:'CSS'
+  },
+  {
+    icon:'react',
+    name:'React'
+  },
+  {
+    icon:'mysql',
+    name:'Sql'
+  },
+  {
+    icon:'jest',
+    name:'Jest'
+  },
+  {
+    icon:'javascript',
+    name:'JavaScript'
+  },
+  {
+    icon:'github',
+    name:'Github'
+  },
+  {
+    icon:'git',
+    name:'Git'
+  },
+  {
+    icon:'html-five',
+    name:'HTML'
+  },
+]
+
+export default skills;
